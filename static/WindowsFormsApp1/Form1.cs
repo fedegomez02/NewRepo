9bda2da5349b0d248a1c87034237d5a01d9a787b
fedegomez02 <112111459+fedegomez02@users.noreply.github.com>
2022-11-04 14:08:08 -0300

    Add files via upload

---
+++ static/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using static1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                Empleado empleado1 = new Empleado(0, "fede", "gomez");
+
+                MessageBox.Show(Empleado.Comision.ToString());
+
+                Empleado empleado2 = new Empleado(1, "Carlos ", "perez");
+                Empleado.Comision = 3000;
+
+                MessageBox.Show(Empleado.Comision.ToString());
+
+                Empleado empleado3 = new Empleado(2, "guillermo", "Francella");
+                Empleado.Comision = 3500;
+
+                MessageBox.Show(Empleado.Comision.ToString());
+
+                Empleado empleado4 = new Empleado(1, "Ricardo ", "Darin");
+                Empleado.Comision = 3700;
+
+                MessageBox.Show(Empleado.Comision.ToString());
+            }
+    }
+}
---
+++ static/static/Empleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static1
+{
+    public class Empleado
+{
+
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        private static int _comision;
+
+        public static int Comision
+        {
+            get { return _comision; }
+            set { _comision = value; }
+        }
+
+
+        public Empleado(int id, string nombre, string apellido)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+
+        }
+
+        public Empleado() { }
+
+    }
+}
